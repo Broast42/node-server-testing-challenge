b66880bce7149f274fb1c7d876a39b2b3655e203
rdowd <rjarrettdowd@yahoo.com>
2020-05-14 19:55:09 -0400

    Added index.js and created server with welcome endpoint.

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express")
+
+const server = express()
+
+const PORT = process.env.PORT || 8888
+
+server.use(express.json())
+
+//routes
+
+server.get("/", (req, res) => {
+	res.json({
+		message: "Welcome server is running."
+	})
+})
+
+//default error
+server.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: "Something went wrong",
+	})
+})
+
+if (!module.parent) {
+	server.listen(PORT, () => {
+		console.log(`Server is listening on port ${PORT}`)
+	})
+}
